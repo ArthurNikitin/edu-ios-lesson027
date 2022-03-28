6f7214a780fd9d349f56f21f76da4e6e7a4e3bad
ArthurNikitin <Arthur.V.Nikitin@gmail.com>
2022-03-28 10:57:20 +0300

    Create xcode project

---
+++ L027 - I'm poor/L027 - I'm poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L027 - I'm poor
+//
+//  Created by Arthur on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
